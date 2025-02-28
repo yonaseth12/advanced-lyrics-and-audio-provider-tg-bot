7676b77e3ed65a9fb5b3a0c678e23d855b0e6d70
Yonas W <yonaseth12@gmail.com>
2025-02-28 14:04:13 +0300

    configure all handlers to main bot

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,16 @@
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from handlers.command_handlers import start_command, lyrics_command
+from handlers.message_handlers import handle_message
+from handlers.callback_handler_audio import callback_handler_audio
+from handlers.callback_handler_main import callback_handler_main
 import config
 
 if __name__ == "__main__":
     app=Application.builder().token(config.TELEGRAM_BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("lyrics", lyrics_command))
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_handler(CallbackQueryHandler(callback_handler_audio, pattern="^AUDIOCONTENT:"))
+    app.add_handler(CallbackQueryHandler(callback_handler_main))	#Fallback Handler
     
     app.run_polling(poll_interval=2)
